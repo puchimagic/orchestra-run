c5cf36a397bdb6c99a34f94cfb38d7daedf7e78c
jack1412kid <159871430+jack1412kid@users.noreply.github.com>
2025-09-26 15:33:48 +0900

    音声を追加

--- soundPlayer.js
+++ soundPlayer.js
@@ -4,6 +4,11 @@ export class SoundPlayer {
       jump: new Audio("./sound/game/track01.wav"),
       score: new Audio("./sound/game/track02.wav"),
       gameOver: new Audio("./sound/game/track03.wav"),
+      game_bgm: new Audio("./sound/game/game_bgm.wav"),
+      gameover_bgm: new Audio("./sound/game/gameover_bgm.wav"),
+      home_bgm: new Audio("./sound/game/home_bgm.wav"),
+      ki_patan: new Audio("./sound/game/ki_patan.wav"),
+
     };
     this.sounds = {}; // loadSoundでロードした音源を格納するオブジェクト
   }
